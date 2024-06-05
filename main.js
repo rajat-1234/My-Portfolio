62997c72843c92951c71eccdd240881ab7bd921b
rajat-1234 <rajatdhatterwal687@gmail.com>
2024-06-05 12:23:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:[" Ethical Hacking" , "CyberSecurity", "Secure Netwok"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
